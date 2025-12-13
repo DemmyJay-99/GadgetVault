e70fb0583813f10582150a7664eb0afd5d3ffeda
DemmyJay <demmyjyd@gmail.com>
2025-12-13 18:55:06 +0100

    commit

--- gadget/src/Products/Card/Card.jsx
+++ gadget/src/Products/Card/Card.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from 'react';
 import styles from './Card.module.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faShoppingCart } from '@fortawesome/free-solid-svg-icons';
+import { faMobile, faShoppingCart, faLaptop, faCamera, faHeadphones } from '@fortawesome/free-solid-svg-icons';
 
 function Card(){
     const [products, setProducts] = useState([])
@@ -28,7 +28,38 @@ function Card(){
         <>
                     {products.map(product => (
                     <div key={product.id} className={styles.card}>
-                        <div className={styles.cardTop}></div>
+                        {(()=>{switch (product.category) {
+                            case "Smartphone":
+                                return <div className={styles.cardTop}>
+                                        <FontAwesomeIcon icon={faMobile} />
+                                        </div>
+                                break;
+                            case "Computing":
+                                return <div className={styles.cardTop}>
+                                    <FontAwesomeIcon icon={faLaptop} />
+                                    </div>
+                                break;
+                            case "Wearable":
+                                return <div className={styles.cardTop}/>
+                                break;
+                            case "Tablet":
+                                return <div className={styles.cardTop}/>
+                                break;
+                            case "Photography":
+                                return <div className={styles.cardTop}>
+                                    <FontAwesomeIcon icon={faCamera}/>
+                                    </div>
+                                break;
+                            case "Audio":
+                                return <div className={styles.cardTop}>
+                                    <FontAwesomeIcon icon={faHeadphones} />
+                                </div>
+                                break;
+                        
+                            default:
+                                break;
+                            }
+                        })()}
                         <div className={styles.cardBottom}>
                             <span className={styles.category}>{product.category}</span>
                             <span className={styles.name}>{product.name}</span>
